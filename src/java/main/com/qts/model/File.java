426f9993d8b902fdfffa7e217d64d98ec1b8fc64
Anil Ram <anilram.laxmisetti@qison.com>
2013-12-02 17:58:22 +0530

    Added User1 and File.

---
+++ src/java/main/com/qts/model/File.java
@@ -0,0 +1,92 @@
+package com.qts.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+//import org.hibernate.annotations.Cascade;
+
+@Entity
+@Table(name="FILE")
+public class File extends AbstractObject{
+	@Id
+	@GeneratedValue
+	@Column(name="ID")
+	private long id;
+	@Column(name="NAME")
+	private String name;
+	@Column(name="PATH")
+	private String path;
+	@Column(name="SERVER_URL")
+	private String serverURL;
+	@OneToOne(mappedBy="photoFile",cascade=CascadeType.ALL)
+	private User1 user;
+	public File() {
+		
+	}
+	
+	public File(String name, String path, String serverURL) {
+		super();
+		this.name = name;
+		this.path = path;
+		this.serverURL = serverURL;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getServerURL() {
+		return serverURL;
+	}
+
+	public void setServerURL(String serverURL) {
+		this.serverURL = serverURL;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public User1 getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(User1 user) {
+		this.user = user;
+	}
+
+	@Override
+	public int getObjectType() {
+		
+		return 0;//--------
+	}
+	
+
+}
